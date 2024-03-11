63b24de461e35a08d3af558f8c92fef17b31e71d
Agung Pratama <agp.csui08@gmail.com>
2024-03-11 12:00:46 +0700

    Add graceful shutdown on nodejs

--- start.js
+++ start.js
@@ -5,3 +5,23 @@ const port = 3000;
 const server = app.listen(port, ()=>{
   console.log(`express is running on port ${server.address().port}`)
 });
+
+process.on('SIGTERM', ()=>{
+  console.log("Receive SIGTERM signal, graceful shutdown...")
+  const timeout = 5000; // msec
+  setTimeout(()=>{
+    console.warn("forced shutdown after timeout")
+    process.exit(1)
+
+  }, timeout);
+  
+  server.close((err) => {
+    TimeRanges.s
+    if (err) {
+      console.error("error on closing server:", err)
+      process.exit(1)
+    }
+    console.log("server closed succesfully.")
+    process.exit(0)
+  });
+});
