26d7ea0f109c067c61796db1c5b983c851c906b8
Agung Pratama <agp.csui08@gmail.com>
2024-03-11 11:12:08 +0700

    Simplify a bit according to some tutorial

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
-const routes = require('./routes/index');
-
 const app = express();
-app.use('/', routes);
+const router = express.Router();
+
+const viewPath = __dirname + "/views/";
+
+router.use(function(req, res, next){
+  console.log(req.method, req.path);
+  next();
+});
+
+
+router.get("/", function(req, res) {
+  res.sendFile(viewPath+"index.html")
+})
+
+router.get("/sharks", function(req, res) {
+  res.sendFile(viewPath+"sharks.html")
+})
+
+app.use(express.static(viewPath))
+app.use("/", router);
 
 module.exports = app;
--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 
 const app = require('./app');
+const port = 3000;
 
-const server = app.listen(3000, ()=>{
+const server = app.listen(port, ()=>{
   console.log(`express is running on port ${server.address().port}`)
 });
